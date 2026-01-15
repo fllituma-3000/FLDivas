8edc440635ac3aca2ce98887319ec3ff8486d5f8
fllituma-3000 <fllituma@sudamericano.edu.ec>
2026-01-14 21:54:48 -0500

    Actualizar proyecto FLDivas: agregar store.page.html y cambios recientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'inicio',
+    loadComponent: () => 
+      import('./page/inicio/inicio.page').then(m => m.InicioPage)
   },
+  {
+    path: 'store',
+    loadComponent: () => 
+      import('./page/store/store.page').then(m => m.StorePage)
+  }
 ];
